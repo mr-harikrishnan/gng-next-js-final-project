7cca12b61ad41544bb49532a1a8c95ce0e6e4f97
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-07-29 16:09:45 +0530

    added 4 number images

--- src/app/(root)/component/servicesSection/ServiceCard.tsx
+++ src/app/(root)/component/servicesSection/ServiceCard.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import no1Image from "@/assets/services/number1Image.png"
+import no2Image from "@/assets/services/number2Image.png"
+import no3Image from "@/assets/services/number3Image.png"
+import no4Image from "@/assets/services/number4Image.png"
 
 function ServiceCard() {
   return (
     <div className='w-full grid grid-cols-2 gap-8 place-content-center place-items-center h-screen '>
 
       <div className=' border py-10 p-2 flex flex-col -mt-12 '>
-        <h1 className='font-morebold text-left'>1</h1>
+        <img src={no1Image.src} alt="" />
         <h1 className='font-morebold text-left'>Skilled Lecturers</h1>
         <p>Cras ornare sagittis tortor a facilisis nteger augue quam, placerat.</p>
       </div>
