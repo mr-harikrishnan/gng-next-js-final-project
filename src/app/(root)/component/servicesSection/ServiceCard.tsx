1bb8d0c0e16ce9b1d738528fbede6ae9f11439b6
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-07-29 18:00:56 +0530

    Services Page completed

--- src/app/(root)/component/servicesSection/ServiceCard.tsx
+++ src/app/(root)/component/servicesSection/ServiceCard.tsx
@@ -5,34 +5,55 @@ import no3Image from "@/assets/services/number3Image.png"
 import no4Image from "@/assets/services/number4Image.png"
 
 function ServiceCard() {
+
+  const serviceCardData = [
+    {
+      img: no1Image,
+      headline: "Skilled Lecturers",
+      desc: "Cras ornare sagittis tortor a facilisis nteger augue quam, placerat."
+    },
+    {
+      img: no2Image,
+      headline: "Book Library",
+      desc: "Cras ornare sagittis tortor a facilisis nteger augue quam, placerat."
+    },
+    {
+      img: no3Image,
+      headline: "Online Classes",
+      desc: "Cras ornare sagittis tortor a facilisis nteger augue quam, placerat."
+    },
+    {
+      img: no4Image,
+      headline: "Skilled Lecturers",
+      desc: "Cras ornare sagittis tortor a facilisis nteger augue quam, placerat."
+    }
+  ]
+
+
+
+
   return (
-    <div className='w-full  grid grid-cols-1 gap-8 place-content-center place-items-center  '>
+    <div className='w-full  grid grid-cols-1 gap-8 place-content-center place-items-center 
+    sm:grid-cols-2 sm:gap-6 '>
+
+      {
+        serviceCardData.map((data, index) =>
+
+          <div className={` border border-white ${index == 0 ? "bg-white " : "bg-[#387975] text-white"}  rounded-lg p-2 flex flex-col items-left px-4
+      sm: ${index % 2 == 0 ? "-mt-8" : "mt-8"} sm:pb-10 `}>
+            <img
+              className='w-14 mt-6'
+              src={data.img.src} alt="" />
+            <h1 className='font-morebold  mt-4 text-[22px]'>{data.headline}</h1>
+            <p className='text-center  mt-4 mb-9 sm:mb-0 sm:text-left'>{data.desc}</p>
+          </div>
+        )
+      }
 
 
-      <div className=' border py-10 p-2 flex flex-col -mt-12 '>
-        <img src={no1Image.src} alt="" />
 
-        <h1 className='font-morebold text-left'>Skilled Lecturers</h1>
-        <p>Cras ornare sagittis tortor a facilisis nteger augue quam, placerat.</p>
-      </div>
 
-      <div className=' border py-10 p-2 flex flex-col '>
-        <h1 className='font-morebold text-left'>1</h1>
-        <h1 className='font-morebold text-left'>Skilled Lecturers</h1>
-        <p>Cras ornare sagittis tortor a facilisis nteger augue quam, placerat.</p>
-      </div>
 
-      <div className=' border py-10 p-2 flex flex-col -mt-12'>
-        <h1 className='font-morebold text-left'>1</h1>
-        <h1 className='font-morebold text-left'>Skilled Lecturers</h1>
-        <p>Cras ornare sagittis tortor a facilisis nteger augue quam, placerat.</p>
-      </div>
-
-      <div className=' border py-10 p-2 flex flex-col '>
-        <h1 className='font-morebold text-left'>1</h1>
-        <h1 className='font-morebold text-left'>Skilled Lecturers</h1>
-        <p>Cras ornare sagittis tortor a facilisis nteger augue quam, placerat.</p>
-      </div>
 
     </div>
   )
